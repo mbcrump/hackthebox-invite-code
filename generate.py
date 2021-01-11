d6958b154372e4f1a1e069cc9914d8b1d0c1d7e4
Michael Crump <mbcrump29@gmail.com>
2021-01-11 11:01:45 -0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,6 +4,7 @@
 
 import requests
 import base64
+import datetime
 
 url = 'https://www.hackthebox.eu/api/invite/generate'
 headers = {'content-type': 'application/json', 'user-agent': 'I bet you wish you knew'}
@@ -13,4 +14,12 @@ value = data['data']['code']
 
 base = base64.b64decode(value)
 
-print ("Your hackthebox invite code is : " + base.decode('utf-8'))
+x = datetime.datetime.now()
+
+print ('<link rel="stylesheet" href="https://mbcrump.github.io/runpython2/mystyle.css">')
+print ('<main>')
+print ('<ol class="gradient-list">')
+print ('<li>')
+print ("Your hackthebox invite code is : " + base.decode('utf-8') + "Generated on: " + x)
+print ('</li>')
+
